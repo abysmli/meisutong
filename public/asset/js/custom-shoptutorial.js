ad64b7df1de8c6c22f10c6b370832352e7268640
Li, Yuan <abysmli@gmail.com>
2017-09-17 03:12:55 +0200

    fix bug

---
+++ public/asset/js/custom-shoptutorial.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+    var $grid = $('.grid').isotope({
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows'
+    });
+
+    // bind filter button click
+    $('.filters-button-group').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        // use filterFn if matches value
+        $grid.isotope({ filter: filterValue });
+    });
+});
